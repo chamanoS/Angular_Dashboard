c19e10c7378dfc5b4dce1717dd9af9d2ace7bed6
Chamano Ramashia <chamano.sr@gmail.com>
2021-08-12 12:36:14 +0200

    adding components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DefaultComponent } from './layouts/default/default.component';
 import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { PostsComponent } from './modules/posts/posts.component';
 
 const routes: Routes = [
   {
     path:"", component:DefaultComponent,
     children:[{
       path:"", component: DashboardComponent
-    }]
-  }
+    },{
+      path:"posts", component:PostsComponent
+      }]
+  },
+
 ];
 
 @NgModule({
@@ -17,3 +21,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+ 
--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -3,17 +3,21 @@ import { CommonModule } from '@angular/common';
 import { DashboardComponent } from 'src/app/modules/dashboard/dashboard.component';
 import { DefaultComponent } from './default.component';
 import { RouterModule } from '@angular/router';
+import { PostsComponent } from 'src/app/modules/posts/posts.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 
 @NgModule({
   declarations: [
     DefaultComponent,
-    DashboardComponent
+    DashboardComponent,
+    PostsComponent,
   ],
   imports: [
     CommonModule,
-    RouterModule 
+    RouterModule,
+    SharedModule
   ]
 })
 export class DefaultModule { }
