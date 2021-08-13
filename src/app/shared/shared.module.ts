96283e95b7ec4196f0199cbb03741945350159a6
Chamano Ramashia <chamano.sr@gmail.com>
2021-08-13 18:33:40 +0200

    adding the header

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,7 +3,9 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
-
+import {MatDividerModule} from '@angular/material/divider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
@@ -14,6 +16,9 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
   ],
   imports: [
     CommonModule,
+    MatDividerModule,
+    MatToolbarModule,
+    MatIconModule,
   ],
   exports:[
     HeaderComponent,
